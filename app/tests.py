addcdbc36e7b4aeb52fc767cf247bba01fd8d537
Diego Chacón <dchacon130@gmail.com>
2023-06-15 16:16:59 -0500

    first automation

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Create your tests here.
+driver = webdriver.Firefox()
+
+try:
+    driver.get('http://127.0.0.1:9000/')
+    welcome_message = driver.find_element(by=By.XPATH, value='/html[1]/body[1]/main[1]/h1[1]').text
+    print(welcome_message)
+    assert welcome_message == 'The install worked successfully! Congratulations'
+except Exception as ex:
+    print('Error: ', ex)
+finally:
+    driver.quit()
+
+
